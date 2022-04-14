260478ef3079436e96bc06ff9340c06b044accf6
Tanya Lamontagne <tanyalamontagne@Tanyas-MacBook-Air.local>
2022-04-13 20:12:24 -0400

    Pushing for github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-// next.config.js
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/vbelectriquesnextjs/' : ''
+  assetPrefix: isProd ? '/vbelectrique/' : ''
 }
