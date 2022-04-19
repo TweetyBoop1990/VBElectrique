5363cc1e536d9494c4a72e101a28d49630e60b73
Tanya Lamontagne <25286291+TweetyBoop1990@users.noreply.github.com>
2022-04-19 19:58:27 -0400

    Dont lose it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import ReactGA from "react-ga";
 import $ from "jquery";
 import "./App.css";
@@ -8,6 +8,8 @@ import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Portfolio from "./Components/Portfolio";
+import { CookiesProvider } from 'react-cookie';
+import './i18n/i18n';
 
 class App extends Component {
   constructor(props) {
@@ -26,10 +28,10 @@ class App extends Component {
       url: "./resumeData.json",
       dataType: "json",
       cache: false,
-      success: function(data) {
+      success: function (data) {
         this.setState({ resumeData: data });
       }.bind(this),
-      error: function(xhr, status, err) {
+      error: function (xhr, status, err) {
         console.log(err);
         alert(err);
       }
@@ -42,14 +44,18 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Header data={this.state.resumeData.main} />
-        <About data={this.state.resumeData.main} />
-        <Resume data={this.state.resumeData.resume} />
-        <Portfolio data={this.state.resumeData.portfolio} />
-        <Contact data={this.state.resumeData.main} />
-        <Footer data={this.state.resumeData.main} />
-      </div>
+      <CookiesProvider>
+        <Suspense fallback={<span>Loading...</span>}>
+          <div className="App">
+            <Header data={this.state.resumeData.main} />
+            <About data={this.state.resumeData.main} />
+            <Resume data={this.state.resumeData.resume} />
+            <Portfolio data={this.state.resumeData.portfolio} />
+            <Contact data={this.state.resumeData.main} />
+            <Footer data={this.state.resumeData.main} />
+          </div>
+        </Suspense>
+      </CookiesProvider>
     );
   }
 }
