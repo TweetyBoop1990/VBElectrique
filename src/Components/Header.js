785642f99849623b1b642f4ef4572ae053714c90
Tanya Lamontagne <25286291+TweetyBoop1990@users.noreply.github.com>
2022-04-18 23:51:13 -0400

    Dont lose it

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,71 +1,90 @@
-import React, { Component } from "react";
-import ParticlesBg from "particles-bg";
+import React from "react";
 import Fade from "react-reveal";
+import ReactPlayer from 'react-player';
+import heroVideo from './istockphoto-1266078247-640_adpp_is.mp4';
+import { useTranslation } from 'react-i18next';
+import LangSelector from './LanguageSwitcher/LangSelector';
 
-class Header extends Component {
-  render() {
-    if (!this.props.data) return null;
+const Header = (props) => {
+  const { t } = useTranslation();
 
-    const project = this.props.data.project;
-    const github = this.props.data.github;
-    const name = this.props.data.name;
-    const description = this.props.data.description;
+  if (!props.data) return null;
 
-    return (
-      <header id="home">
-        <ParticlesBg type="circle" bg={true} />
+  const project = props.data.project;
+  const github = props.data.github;
+  const name = props.data.name;
+  const description = props.data.description;
 
-        <nav id="nav-wrap">
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-            Show navigation
-          </a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">
-            Hide navigation
-          </a>
+  return (
+    <header id="home">
+      <ReactPlayer
+        url={heroVideo}
+        playing
+        loop
+        muted
+        width="100%"
+        height="100%"
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: 0,
+          top: 0,
+          transform: "scaleX(1.4) scaleY(1.4)"
+        }}
+      />
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+          Show navigation
+        </a>
+        <a className="mobile-btn" href="#home" title="Hide navigation">
+          Hide navigation
+        </a>
+        <ul id="nav" className="nav">
+          <li className="current">
+            <a className="smoothscroll" href="#home">
+              {t('HOME.TITLE')}
+            </a>
+          </li>
 
-          <ul id="nav" className="nav">
-            <li className="current">
-              <a className="smoothscroll" href="#home">
-                Home
-              </a>
-            </li>
+          <li>
+            <a className="smoothscroll" href="#about">
+              {t('ABOUT.TITLE')}
+            </a>
+          </li>
 
-            <li>
-              <a className="smoothscroll" href="#about">
-                About
-              </a>
-            </li>
+          <li>
+            <a className="smoothscroll" href="#resume">
+              {t('SERVICES.TITLE')}
+            </a>
+          </li>
 
-            <li>
-              <a className="smoothscroll" href="#resume">
-                Resume
-              </a>
-            </li>
+          <li>
+            <a className="smoothscroll" href="#portfolio">
+              Works
+            </a>
+          </li>
 
-            <li>
-              <a className="smoothscroll" href="#portfolio">
-                Works
-              </a>
-            </li>
+          <li>
+            <a className="smoothscroll" href="#contact">
+              Contact
+            </a>
+          </li>
+          <li className="">
+            <LangSelector />
+          </li>
+        </ul>
+      </nav>
 
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-
-        <div className="row banner">
-          <div className="banner-text">
-            <Fade bottom>
-              <h1 className="responsive-headline">{name}</h1>
-            </Fade>
-            <Fade bottom duration={1200}>
-              <h3>{description}.</h3>
-            </Fade>
-            <hr />
-            <Fade bottom duration={2000}>
+      <div className="row banner">
+        <div className="banner-text">
+          <Fade bottom>
+            <h1 className="responsive-headline">{name}</h1>
+          </Fade>
+          <Fade bottom duration={1200}>
+            <h3>{description}.</h3>
+          </Fade>
+          <hr />
+          {/* <Fade bottom duration={2000}>
               <ul className="social">
                 <a href={project} className="button btn project-btn">
                   <i className="fa fa-book"></i>Project
@@ -74,18 +93,17 @@ class Header extends Component {
                   <i className="fa fa-github"></i>Github
                 </a>
               </ul>
-            </Fade>
-          </div>
+            </Fade> */}
         </div>
+      </div>
 
-        <p className="scrolldown">
-          <a className="smoothscroll" href="#about">
-            <i className="icon-down-circle"></i>
-          </a>
-        </p>
-      </header>
-    );
-  }
+      <p className="scrolldown">
+        <a className="smoothscroll" href="#about">
+          <i className="icon-down-circle"></i>
+        </a>
+      </p>
+    </header>
+  );
 }
 
 export default Header;
---
+++ src/Components/LanguageSwitcher/LangSelector.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import FormControlLabel from '@mui/material/FormControlLabel';
+import MaterialUISwitch from './MaterialUiSwitchConfig';
+
+const LangSelector = () => {
+  const [isChecked, setChecked] = useState(true);
+  const { i18n } = useTranslation()
+
+  const changeLanguageHandler = (lang) =>
+  {
+    i18n.changeLanguage(lang)
+  }
+
+  const handleLanguageChange = evt => {
+    setChecked(evt.target.checked);
+  
+    if (evt.target.checked === true) {
+      changeLanguageHandler('fr')
+      } else { 
+        changeLanguageHandler('en')
+      }
+  };
+
+  return (
+    <FormControlLabel
+      control={
+        <MaterialUISwitch
+          onChange={handleLanguageChange}
+          checked={isChecked}
+        />
+      }
+    />
+  );
+};
+
+export default LangSelector;
